3200920bebb6fde07343130378ffa1a648c4ac14
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2023-02-09 21:19:00 +0000

    02/09

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from random import randint
 dealer = randint(0,11)
 card1 = randint(0,11)
 card2 = randint(0,11)
-
-
+others = ["queen","king","ace","joker"]
 
 def Game(card1, card2):
   print("Welcome to this game of Blackjack, your objective is to get 21 or get the closest number to 21 without going over but getting something higher than the dealer good luck with the game you'll need it")
   total = card1 + card2
-  while total < 21:
+  while total < 21 :
     print(" ")
     print("dealers hand:",dealer)
     hand = ("your hand, card 1: ", card1, "card 2: ", card2 )
